66b0ca07f78fd49fd2cbbc9f3d0746369c6f2393
Jordan Deam <jordan.deam@gmail.com>
2018-04-03 22:27:02 -0700

    Started laying out basic app structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import LoginView from './components/LoginView';
+import SignupView from './components/SignupView';
+import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import 'bulma/css/bulma.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+const App = () => {
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/login" component={ LoginView } />
+          <Route path="/signup" component={ SignupView } />
+        </Switch>
+        <Footer />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
