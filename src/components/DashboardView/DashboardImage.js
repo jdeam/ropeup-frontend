69e598891afcae4e2672394bd1ec92be96425d87
Jordan Deam <jordan.deam@gmail.com>
2018-04-10 23:37:35 -0700

    Added upload to Cloudinary on DashboardImage

--- src/components/DashboardView/DashboardImage.js
+++ src/components/DashboardView/DashboardImage.js
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+import axios from 'axios';
 import FontAwesome from 'react-fontawesome';
+import { fetchUser } from '../../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import moment from 'moment';
 import './Dashboard.css';
 
+const BaseURL = process.env.REACT_APP_BASE_URL;
 const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
 const UPLOAD_URL = process.env.REACT_APP_UPLOAD_URL;
 
 class DashboardImage extends Component {
-  state = {
-    uploadedFileUrl: ''
-  };
-
-  onImageDrop = (files) => {
-    console.log(files);
-    this.setState({ uploadededFile: files[0] });
-    // this.handleImageUpload(files[0]);
+  onDrop = (files) => {
+    this.setState({ uploadedFile: files[0] });
+    this.handleImageUpload(files[0]);
   }
 
   handleImageUpload = (file) => {
-    console.log(UPLOAD_URL);
-    console.log(UPLOAD_PRESET);
-    let upload = request.post(UPLOAD_URL)
+    const upload = request.post(UPLOAD_URL)
       .field('upload_preset', UPLOAD_PRESET)
-      .field('file', file);
-
-    upload.end((err, response) => {
-      if (err) console.error(err);
-      if (response.body.secure_url !== '') this.setState({
-        uploadedFileUrl: response.body.secure_url
+      .field('file', file)
+      .end( async (err, res) => {
+        if (err) console.log(err);
+        if (res.body.secure_url) {
+          const uploadBody = { img_url: res.body.secure_url };
+          const { token, user, fetchUser } = this.props;
+          const response = await axios.patch(
+            `${BaseURL}/users/${user.id}`,
+            uploadBody,
+            { headers: { token } }
+          );
+          if (response.status === 200) fetchUser();
+        }
       });
-    });
   };
 
   getUserAge = () => {
@@ -58,8 +62,8 @@ class DashboardImage extends Component {
               <Dropzone
                 className="dashboard-dropzone"
                 multiple={ false }
-                accept="image/jpg,image/png"
-                onDrop={ this.onImageDrop }
+                accept="image/*"
+                onDrop={ this.onDrop }
               >
                 <FontAwesome
                   className="fa-4x"
@@ -77,4 +81,16 @@ class DashboardImage extends Component {
   }
 }
 
-export default DashboardImage;
+const mapStateToProps = (state) => ({
+  token: state.token,
+  user: state.user
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  fetchUser
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardImage);
